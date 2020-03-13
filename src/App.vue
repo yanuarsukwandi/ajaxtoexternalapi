bb8c60d83a4eb1ea005d3d7ffc71ad8261b1ba83
Yanuar Gandhira <yanuar.sukwandi@gmail.com>
2020-03-13 17:24:32 +0700

    commit5

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Menu></Menu>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Menu from "@/components/Menu.vue";
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
+  components: {
+    Menu
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({})
+};
+</script>
